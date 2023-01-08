9e58c03a58a67541f6a0185ca780e8e0458402fe
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-08 15:23:42 +1100

    Create porportionate grid

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector('#canvas');
 let numberOfSquares = 16;
+canvas.setAttribute('style', 
+`grid-template-columns: repeat(${numberOfSquares}, 1fr);
+grid-auto-rows: 1fr;`);
 
 for (let i = 0; i < (Math.pow(numberOfSquares, 2)); i++) {
     const pixels = document.createElement('div');
