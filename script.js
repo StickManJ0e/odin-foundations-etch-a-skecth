1271baebde8ccd9762d841f4584672e10a830186
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-08 15:54:42 +1100

    Change colour of pixels on hover

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const canvas = document.querySelector('#canvas');
 let numberOfSquares = 16;
-canvas.setAttribute('style', 
-`grid-template-columns: repeat(${numberOfSquares}, 1fr);
-grid-auto-rows: 1fr;`);
 
-for (let i = 0; i < (Math.pow(numberOfSquares, 2)); i++) {
-    const pixels = document.createElement('div');
-    pixels.classList.add('pixels');
-    canvas.appendChild(pixels);
+//Create divs to represent each pixel
+//Change the canvas dimensions accoriding to the chosen # of squares
+function createGrid(numberOfSquares) {
+    canvas.setAttribute('style', 
+    `grid-template-columns: repeat(${numberOfSquares}, 1fr);
+    grid-auto-rows: 1fr;`);
+    
+    for (let i = 0; i < (Math.pow(numberOfSquares, 2)); i++) {
+        const pixels = document.createElement('div');
+        pixels.classList.add('pixels');
+        canvas.appendChild(pixels);
+    };
 }
+createGrid(numberOfSquares);
+
+const pixels = document.querySelectorAll('.pixels');
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mousemove', () => {
+        pixel.classList.add('colouredPixel');
+    });
+})
